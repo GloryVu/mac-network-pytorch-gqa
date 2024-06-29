284927e310762660ac793d23d976c894da7454e3
Vinh Vu <61405339+GloryVu@users.noreply.github.com>
2024-06-29 16:13:16 +0700

    Update train.py

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ from dataset import CLEVR, collate_data, transform, GQA
 from model_gqa import MACNetwork
 
 batch_size = 64
-n_epoch = 100
+n_epoch = 50
 dim_dict = {'CLEVR': 2048,
             'gqa': 2048}
 
@@ -129,7 +129,7 @@ if __name__ == '__main__':
     accumulate(net_running, net, 0)
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(net.parameters(), lr=1e-4)
+    optimizer = optim.Adam(net.parameters(), lr=1.5e-4)
     curr_val_acc = 0
     for epoch in range(n_epoch):
         train(epoch, dataset_type)
