030ec442bd31a7c0262f3ac5a0781cb734baa520
Vinh Vu <61405339+GloryVu@users.noreply.github.com>
2024-06-29 19:21:14 +0700

    Update train.py

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ from dataset import CLEVR, collate_data, transform, GQA
 from model_gqa import MACNetwork
 
 batch_size = 128
-n_epoch = 25
+n_epoch = 50
 dim_dict = {'CLEVR': 2048,
             'gqa': 2048}
 
